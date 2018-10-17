ad240ec1fe7014bfd1add1bd21f6aca79e0e0039
Martin Disch <martindisch@gmail.com>
2018-10-17 10:18:00 +0200

    Store & retrieve sensor data with DB
    TG-19 #closed
    TG-21 #closed

--- main.js
+++ main.js
@@ -3,51 +3,100 @@ const log = require('debug')('api');
 const Koa = require('koa');
 const Router = require('koa-router');
 const cors = require('@koa/cors');
+const Influx = require('influx');
 const mqtt = require('./mqtt.js').connect();
 
 const app = new Koa();
 const router = new Router();
-
-/*
- * Global variables to store latest values of sensors until we have a DB
- */
-const sensorData = {
-  lastLight: {},
-  lastHumidity: 0,
-  lastTemp: {},
-  lastAirQuality: {}
-};
+const influx = new Influx.InfluxDB({
+  host: 'db',
+  database: process.env.INFLUXDB_DB,
+  schema: [
+    {
+      measurement: 'light_intensity',
+      fields: {
+        red: Influx.FieldType.INTEGER,
+        green: Influx.FieldType.INTEGER,
+        blue: Influx.FieldType.INTEGER,
+        clear: Influx.FieldType.INTEGER
+      },
+      tags: []
+    },
+    {
+      measurement: 'humidity',
+      fields: {
+        humidity: Influx.FieldType.INTEGER
+      },
+      tags: []
+    },
+    {
+      measurement: 'temperature',
+      fields: {
+        temperature: Influx.FieldType.FLOAT
+      },
+      tags: []
+    },
+    {
+      measurement: 'air_quality',
+      fields: {
+        eco2: Influx.FieldType.INTEGER,
+        tvoc: Influx.FieldType.INTEGER
+      },
+      tags: []
+    }
+  ]
+});
 
 /*
  * MQTT handlers receiving and storing sensor data
  */
 
-mqtt.on('light_intensity', (data) => {
-  sensorData.lastLight = {
+mqtt.on('light_intensity', async (data) => {
+  const light = {
     red: data.readUInt16LE(0),
     green: data.readUInt16LE(2),
     blue: data.readUInt16LE(4),
     clear: data.readUInt16LE(6)
   };
+  await influx.writePoints([
+    {
+      measurement: 'light_intensity',
+      fields: light,
+    }
+  ]);
 });
 
-mqtt.on('humidity', (data) => {
-  sensorData.lastHumidity = data.readUInt8(0);
+mqtt.on('humidity', async (data) => {
+  const humidity = data.readUInt8(0);
+  await influx.writePoints([
+    {
+      measurement: 'humidity',
+      fields: { humidity }
+    }
+  ]);
 });
 
-mqtt.on('temperature', (data) => {
-  sensorData.lastTemp = {
-    integer: data.readInt8(0),
-    decimal: data.readUInt8(1),
-    temperature: data.readInt8(0) + (data.readUInt8(1) / 100)
-  };
+mqtt.on('temperature', async (data) => {
+  const temperature = data.readInt8(0) + (data.readUInt8(1) / 100);
+  await influx.writePoints([
+    {
+      measurement: 'temperature',
+      fields: { temperature }
+    }
+  ]);
 });
 
-mqtt.on('air_quality', (data) => {
-  sensorData.lastAirQuality = {
+mqtt.on('air_quality', async (data) => {
+  const airQuality = {
     eco2: data.readUInt16LE(0),
     tvoc: data.readUInt16LE(2)
   };
+  await influx.writePoints([
+    {
+      measurement: 'air_quality',
+      fields: airQuality
+    }
+  ]);
 });
 
 /*
@@ -55,19 +104,23 @@ mqtt.on('air_quality', (data) => {
  */
 
 async function getLight(ctx) {
-  ctx.body = sensorData.lastLight;
+  const rows = await influx.query('SELECT * FROM light_intensity');
+  ctx.body = rows;
 }
 
 async function getHumidity(ctx) {
-  ctx.body = sensorData.lastHumidity;
+  const rows = await influx.query('SELECT * FROM humidity');
+  ctx.body = rows;
 }
 
 async function getTemperature(ctx) {
-  ctx.body = sensorData.lastTemp;
+  const rows = await influx.query('SELECT * FROM temperature');
+  ctx.body = rows;
 }
 
 async function getAirQuality(ctx) {
-  ctx.body = sensorData.lastAirQuality;
+  const rows = await influx.query('SELECT * FROM air_quality');
+  ctx.body = rows;
 }
 
 /*
@@ -75,10 +128,10 @@ async function getAirQuality(ctx) {
  */
 
 router
-  .get('/api/v1/light/latest', getLight)
-  .get('/api/v1/humidity/latest', getHumidity)
-  .get('/api/v1/temperature/latest', getTemperature)
-  .get('/api/v1/air_quality/latest', getAirQuality);
+  .get('/api/v1/light', getLight)
+  .get('/api/v1/humidity', getHumidity)
+  .get('/api/v1/temperature', getTemperature)
+  .get('/api/v1/air_quality', getAirQuality);
 
 app
   .use(cors())
